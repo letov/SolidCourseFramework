fd78839ca6a8cc71be7dd3b3c52f5477e8590c22
ruslan <ruslan.leto@gmail.com>
2022-02-19 03:43:13 +0300

    add-vapor

--- SolidCourseFrameworkTests/GeneratedMocks.swift
+++ SolidCourseFrameworkTests/GeneratedMocks.swift
@@ -1,4 +1,4 @@
-// MARK: - Mocks generated from file: SolidCourseFramework/SolidCourseFramework.swift at 2022-02-18 22:48:47 +0000
+// MARK: - Mocks generated from file: SolidCourseFramework/SolidCourseFramework.swift at 2022-02-19 00:41:47 +0000
 
 //
 //  SolidCourseFramework.swift
